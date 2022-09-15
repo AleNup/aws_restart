c630ca1c25c78da6340e4b1cad475f073dd5b46f
AleNup <113637574+AleNup@users.noreply.github.com>
2022-09-15 16:00:15 -0400

    Add files via upload

---
+++ numeric-data.py
@@ -0,0 +1,21 @@
+print("Python has three numeric types: int, float, and complex")
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + "is for the data type " + str(type(myValue)))
+myvalue=5j
+print(myValue)
+print(type(myvalue))
+print(str(myValue) + "is for the data type " + str(type(myValue)))
+myvalue=True
+print(myvalue)
+print(type(myValue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type "+ str(type(myValue)))
