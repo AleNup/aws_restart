760178e84272c6bd78dcdfeba56a7c10a1c6173a
AleNup <113637574+AleNup@users.noreply.github.com>
2022-09-22 15:08:03 -0400

    Add files via upload

---
+++ problema5.py
@@ -0,0 +1,7 @@
+first = int(input("Ingrese el primer número: "))
+second = int(input("Ingrese el segundo número: "))
+third = int(input("Ingrese el tercer número: "))
+
+output = min(first, second, third)
+
+print("El número menor es: ", output)
