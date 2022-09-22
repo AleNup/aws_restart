b9ac484bbfb38728c2a684c02929f8863cca450c
AleNup <113637574+AleNup@users.noreply.github.com>
2022-09-22 16:08:55 -0400

    Add files via upload

---
+++ problema6.py
@@ -0,0 +1,7 @@
+#Entrada: nombre y apellido
+#Salida: Mensaje indicando las iniciales del nombre completo de la persona
+
+name = input("Nombre: ")
+apellido = input("Apellido: ")
+
+print(name[0] + apellido[0])
